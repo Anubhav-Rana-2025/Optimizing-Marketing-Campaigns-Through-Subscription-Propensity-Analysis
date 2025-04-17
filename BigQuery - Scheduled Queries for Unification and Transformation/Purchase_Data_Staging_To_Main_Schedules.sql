52381aa8f91e51a92db2e24d796f683d99a20775
Anubhav-Rana-2025 <anubhavchetu@gmail.com>
2025-04-17 21:33:13 +0530

    Delete BigQuery - Scheduled Queries for Unification and Transformation/Purchase_Data_Staging_To_Main_Schedules.sql

--- BigQuery - Scheduled Queries for Unification and Transformation/Purchase_Data_Staging_To_Main_Schedules.sql
+++
@@ -1,7 +0,0 @@
-CREATE or Replace TABLE
-customer-marketing-analytics.customer_and_marketing_data_analysis.Purchase_Data_CMA as
-
-
-select purchase_id, customer_id, cast(purchase_date as date) as purchase_date,
-quantity, payment_method, shipping_address,
- shipping_state, product_id from customer-marketing-analytics.customer_and_marketing_data.customer_purchases_data
