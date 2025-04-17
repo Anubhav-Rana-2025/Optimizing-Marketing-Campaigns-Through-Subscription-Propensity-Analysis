b3f6d0a2b3cb360b1ff47d5b0bf037a9b747b9b9
Anubhav-Rana-2025 <anubhavchetu@gmail.com>
2025-04-17 21:34:41 +0530

    Delete BigQuery - Scheduled Queries for Unification and Transformation/CRM_Data_Staging_To_Main_Scheduled.sql

--- BigQuery - Scheduled Queries for Unification and Transformation/CRM_Data_Staging_To_Main_Scheduled.sql
+++
@@ -1,5 +0,0 @@
-CREATE or Replace TABLE
-customer-marketing-analytics.customer_and_marketing_data_analysis.Customer_Data_CMA as
-
-
-select * from customer-marketing-analytics.customer_and_marketing_data.customer_relationship_management_data
